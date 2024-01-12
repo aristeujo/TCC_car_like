9e3a38fdc67f328d85ae2fe138cda182a2bf1508
aristeujo <jasc.rai18@uea.edu.br>
2024-01-12 00:03:06 -0400

    feat: comunicacao com ros funcionando. Enviando os dados dos encoders a cada 100 ms

--- catkin_ws/src/ros_esp32/src/controle_motorDC/controle_motorDC.ino
+++ catkin_ws/src/ros_esp32/src/controle_motorDC/controle_motorDC.ino
@@ -1,3 +1,4 @@
+#define ROSSERIAL_ARDUINO_TCP
 #include "kalmanFilter.h"
 #include "stateSpaceMatrices.h"
 #include "controller.h"
@@ -5,8 +6,14 @@
 #include "motor.h"
 #include "AS5600.h"
 #include "encoder.h"
+#include "WiFi.h"
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include <my_project_msgs/Sensors.h>
 
 void ISR_contador();
+void setupWiFi();
+void cmdVel_to_pwm_cb( const geometry_msgs::Twist &velocity_msg);
 
 #define FORWARD 1
 #define BACKWARD 0
@@ -15,6 +22,16 @@ void ISR_contador();
 #define ENC_IN_A 12 // Fio verde
 #define ENC_IN_B 13 // Fio Amarelo
 #define potPin  34
+
+IPAddress server(192, 168, 0, 30);
+uint16_t serverPort = 11411;
+const char*  ssid = "Seixas_Net";
+const char*  password = "Mayum647";
+
+ros::NodeHandle  nh;
+my_project_msgs::Sensors msg;
+ros::Publisher chatter("/sensors_values", &msg);
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &cmdVel_to_pwm_cb );
 
 Motor motor(19,18,4,22);
 Encoder encoder;
@@ -36,6 +53,13 @@ unsigned long intervalo = 100;
 void setup() {
 
   Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  setupWiFi();
+  nh.getHardware()->setConnection(server, serverPort);
+  nh.initNode();
+  nh.advertise(chatter);
+  nh.subscribe(sub);
+  
   Wire.begin();
   motor.initMotor();
 
@@ -51,7 +75,7 @@ void setup() {
   motor.setAngle(motor.angulo_frente);
   Serial.println("End of Setup");
 
-  delay(2000);
+  delay(1000);
 }
 
 void loop() {
@@ -70,8 +94,16 @@ void loop() {
     // Serial.printf("RPM:%.2f\n", encoder.rpm);
     Serial.printf("RPM:%.2f  AS5600: %.2f\n", rpm, enc_as5600);
     // Serial.printf("r:%.2f  x_hat:%.2f y: %.2f\n", Controller.r(0), states(0), rpm);
+    
+    msg.encoder_eixo = rpm;
+    msg.encoder_as5600_L = enc_as5600;
+    msg.encoder_as5600_R = 110;   
+    chatter.publish(&msg);
+    nh.spinOnce();
+
     timer = millis();
   }
+
 }
 
 void ISR_contador(){
@@ -91,3 +123,23 @@ void ISR_contador(){
     encoder.pulsos_roda--;
   }
 };
+
+void setupWiFi(){  
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) { delay(500);Serial.print("."); }
+   Serial.print("SSID: ");
+   Serial.println(WiFi.SSID());
+   Serial.print("IP:   ");
+   Serial.println(WiFi.localIP());
+   digitalWrite(2, HIGH);
+
+}
+
+void cmdVel_to_pwm_cb( const geometry_msgs::Twist &velocity_msg){
+
+    float right_wheel = (velocity_msg.linear.x + velocity_msg.angular.z ) / 2 ;
+    float left_wheel = (velocity_msg.linear.x - velocity_msg.angular.z ) /2 ;
+
+    Serial.print(left_wheel);Serial.print(" / ");Serial.println(right_wheel);
+
+}
