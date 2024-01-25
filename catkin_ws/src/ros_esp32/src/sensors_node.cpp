4c76a4b38a417144a130e584c690b8299021f3ea
aristeujo <jasc.eai18@uea.edu.br>
2024-01-24 23:02:48 -0400

    feat: Implementacao odometria, mas ainda falta testar.

--- catkin_ws/src/ros_esp32/src/sensors_node.cpp
+++ catkin_ws/src/ros_esp32/src/sensors_node.cpp
@@ -1,25 +1,30 @@
 #include "ros/ros.h"
 #include <tf/transform_broadcaster.h>
 #include "my_project_msgs/Sensors.h"
-#include "nav_msgs/Odometry.h"
+#include <nav_msgs/Odometry.h>
+#include <math.h>
 
 class Sensors_listener
 {
 private:
     //ROS parameters
     double wheelbase_;
+    double wheelRadius_;
 
     const char *topic_sub_ = "/sensors_values";
     float rpm_eixo_;
     float rpm_as5600_R_;
     float rpm_as5600_L_;
 
+    double vL_, vR_;
+
     //odom state
-    double x_, y_, yaw_;
+    double x_, y_, yaw_, theta_;
 
     ros::Subscriber sub_sensors_;
     ros::Publisher odom_pub_;
     tf::TransformBroadcaster odom_broadcaster_;
+    ros::Time current_time_, last_time_;
 
 public:
     Sensors_listener(ros::NodeHandle *nh);
@@ -35,10 +40,27 @@ Sensors_listener::Sensors_listener(ros::NodeHandle *nh)
 }
 
 void Sensors_listener::odometry_calc(){
-    double current_speed = (rpm_as5600_L_+rpm_as5600_R_)/2;
+
+    // Convert rpm to m/s
+    vL_ = (2*M_PI*rpm_as5600_L_*wheelRadius_)/60;;
+    vR_ = (2*M_PI*rpm_as5600_R_*wheelRadius_)/60;
+
+    // calc elapsed time
+    current_time_ = ros::Time::now();
+
+    double dt = (current_time_ - last_time_).toSec();
+
+    double current_speed = (vL_+vR_)/2;
     
-    double x_dot = current_speed * cos(yaw_);
-    double y_dot = current_speed * sin(yaw_);
+    double x_dot = current_speed * cos(theta_);
+    double y_dot = current_speed * sin(theta_);
+
+    // odometry estimation
+    x_ += x_dot*dt;
+    y_ += y_dot*dt;
+    theta_ += (vR_ - vL_)/wheelbase_;
+
+    last_time_ = current_time_;
 }
 
 void Sensors_listener::sensorsCallback(const my_project_msgs::Sensors &msg){
