c44953757396d80e1701267fa4326895935175ce
aristeujo <jasc.eai18@uea.edu.br>
2024-02-22 22:12:43 -0400

    deleted: ws_odom test code removed

--- ws_odom/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aristeujo/TCC/ws_odom/src
-# Build directory: /home/aristeujo/TCC/ws_odom/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("odom_test")
--- ws_odom/build/odom_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aristeujo/TCC/ws_odom/src/odom_test
-# Build directory: /home/aristeujo/TCC/ws_odom/build/odom_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
